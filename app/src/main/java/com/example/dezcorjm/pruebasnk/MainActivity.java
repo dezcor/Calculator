25b666766a23cbe06a74008313d8818893fd2988
dezcor <32680485+dezcor@users.noreply.github.com>
2018-03-09 23:02:41 -0600

    Exceciones Puntos, Cambio de operaciones.

--- app/src/main/java/com/example/dezcorjm/pruebasnk/MainActivity.java
+++ app/src/main/java/com/example/dezcorjm/pruebasnk/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener {
@@ -40,131 +39,214 @@ public class MainActivity extends AppCompatActivity implements android.view.View
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btNueve = (Button) findViewById(R.id.btNueve);
+        btNueve = findViewById(R.id.btNueve);
         btNueve.setOnClickListener(this);
-        btOcho = (Button) findViewById(R.id.btOcho);
+        btOcho =  findViewById(R.id.btOcho);
         btOcho.setOnClickListener(this);
-        btSiete = (Button) findViewById(R.id.btSiete);
+        btSiete =  findViewById(R.id.btSiete);
         btSiete.setOnClickListener(this);
-        btSeis = (Button) findViewById(R.id.btSeis);
+        btSeis =  findViewById(R.id.btSeis);
         btSeis.setOnClickListener(this);
-        btSinco = (Button) findViewById(R.id.btSinco);
+        btSinco =  findViewById(R.id.btSinco);
         btSinco.setOnClickListener(this);
-        btCuatro = (Button) findViewById(R.id.btCuatro);
+        btCuatro = findViewById(R.id.btCuatro);
         btCuatro.setOnClickListener(this);
-        btTres = (Button) findViewById(R.id.btTres);
+        btTres =  findViewById(R.id.btTres);
         btTres.setOnClickListener(this);
-        btDos = (Button) findViewById(R.id.btDos);
+        btDos = findViewById(R.id.btDos);
         btDos.setOnClickListener(this);
-        btUno = (Button) findViewById(R.id.btUno);
+        btUno = findViewById(R.id.btUno);
         btUno.setOnClickListener(this);
-        btCero = (Button) findViewById(R.id.btCero);
+        btCero = findViewById(R.id.btCero);
         btCero.setOnClickListener(this);
-        btPunto = (Button) findViewById(R.id.btPunto);
+        btPunto = findViewById(R.id.btPunto);
         btPunto.setOnClickListener(this);
-        btMult = (Button) findViewById(R.id.btMult);
+        btMult = findViewById(R.id.btMult);
         btMult.setOnClickListener(this);
-        btDiv = (Button) findViewById(R.id.btDiv);
+        btDiv = findViewById(R.id.btDiv);
         btDiv.setOnClickListener(this);
-        btSum = (Button) findViewById(R.id.btSum);
+        btSum = findViewById(R.id.btSum);
         btSum.setOnClickListener(this);
-        btResta = (Button) findViewById(R.id.btResta);
+        btResta = findViewById(R.id.btResta);
         btResta.setOnClickListener(this);
-        btParenteisL = (Button) findViewById(R.id.btParentL);
+        btParenteisL =  findViewById(R.id.btParentL);
         btParenteisL.setOnClickListener(this);
-        btParenteisR = (Button) findViewById(R.id.btParentR);
+        btParenteisR = findViewById(R.id.btParentR);
         btParenteisR.setOnClickListener(this);
-        btDel = (Button) findViewById(R.id.btDel);
+        btDel =  findViewById(R.id.btDel);
         btDel.setOnClickListener(this);
-        btDelAll = (Button) findViewById(R.id.btDelAll);
+        btDelAll = findViewById(R.id.btDelAll);
         btDelAll.setOnClickListener(this);
-        btIgual = (Button) findViewById(R.id.btIgual);
+        btIgual = findViewById(R.id.btIgual);
         btIgual.setOnClickListener(this);
         // Example of a call to a native method
-        tbxX = (EditText) findViewById(R.id.tbxX);
+        tbxX =  findViewById(R.id.tbxX);
         tbxX.setFocusableInTouchMode(false);
         // Example of a call to a native method
-        tbxResult = (EditText) findViewById(R.id.tbxResult);
+        tbxResult = findViewById(R.id.tbxResult);
         tbxResult.setFocusableInTouchMode(false);
     }
 
     @Override
     public void onClick(View view)
     {
-
+        char Mult=0;
+        String Punto;
         switch (view.getId())
         {
             case R.id.btNueve:
                 tbxX.getText().append('9');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btOcho:
                 tbxX.getText().append('8');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btSiete:
                 tbxX.getText().append('7');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btSeis:
                 tbxX.getText().append('6');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btSinco:
                 tbxX.getText().append('5');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btCuatro:
                 tbxX.getText().append('4');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btTres:
                 tbxX.getText().append('3');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btDos:
                 tbxX.getText().append('2');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btUno:
                 tbxX.getText().append('1');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btCero:
                 tbxX.getText().append('0');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                getOperation();
                 break;
             case R.id.btPunto:
-                tbxX.getText().append('.');
+                if(tbxX.getText().length()>0)
+                {
+                    Punto=tbxX.getText().toString();
+                    Mult=Punto.charAt(Punto.length() - 1);
+                    if(Mult=='+' || Mult == '*' || Mult == '/' ||Mult == '-')
+                    {
+                        tbxX.getText().append('0');
+                        tbxX.getText().append('.');
+                    }
+                    else if(Mult=='.')
+                    {
+                        tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
+                        tbxX.getText().append('.');
+                    }
+                    else
+                    {
+                        int count=0;
+                        for(int i=Punto.length()-1;i>0;i--)
+                        {
+                            Mult=Punto.charAt(i);
+                            if(Mult=='+' || Mult == '*' || Mult == '/' ||Mult == '-')
+                            {
+                                break;
+                            }
+                            else if(Mult=='.')
+                            {
+                                count++;
+                            }
+                        }
+                        if(count==0)
+                            tbxX.getText().append('.');
+                    }
+                }
+                else
+                {
+                    tbxX.getText().append('0');
+                    tbxX.getText().append('.');
+                }
+
                 break;
             case R.id.btParentL:
-                tbxX.getText().append(')');
-                tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                if(IsParentesis()==true) {
+                    tbxX.getText().append(')');
+                    getOperation();
+                }
                 break;
             case R.id.btParentR:
                 tbxX.getText().append('(');
                 break;
             case R.id.btMult:
-                tbxX.getText().append('*');
+                if(tbxX.getText().length()>=1) {
+                    Mult = tbxX.getText().charAt(tbxX.getText().length() - 1);
+                    if (Mult == '+' || Mult == '*' || Mult == '/' || Mult == '-') {
+                        Mult = tbxX.getText().charAt(tbxX.getText().length() - 2);
+                        if (Mult == '*' || Mult == '/')
+                            tbxX.getText().delete(tbxX.getText().length() - 2, tbxX.getText().length());
+                        else
+                            tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
+                    }
+                    if(Mult != '(')
+                    tbxX.getText().append('*');
+                }
                 break;
             case R.id.btDiv:
-                tbxX.getText().append('/');
+                if(tbxX.getText().length()>=1) {
+                    Mult = tbxX.getText().charAt(tbxX.getText().length() - 1);
+                    if (Mult == '+' || Mult == '*' || Mult == '/' || Mult == '-') {
+                        Mult = tbxX.getText().charAt(tbxX.getText().length() - 2);
+                        if (Mult == '*' || Mult == '/')
+                            tbxX.getText().delete(tbxX.getText().length() - 2, tbxX.getText().length());
+                        else
+                            tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
+                    }
+                    if(Mult != '(')
+                    tbxX.getText().append('/');
+                }
                 break;
             case R.id.btResta:
+                if(tbxX.getText().length()>0)
+                {
+                    Mult=tbxX.getText().charAt(tbxX.getText().length()-1);
+                    if(Mult=='+'  || Mult == '-')
+                    {
+                        tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
+                    }
+                }
                 tbxX.getText().append('-');
                 break;
             case R.id.btSum:
-                tbxX.getText().append('+');
+                if(tbxX.getText().length()>=1) {
+                    Mult = tbxX.getText().charAt(tbxX.getText().length() - 1);
+                    if (Mult == '+' || Mult == '*' || Mult == '/' || Mult == '-') {
+                        Mult = tbxX.getText().charAt(tbxX.getText().length() - 2);
+                        if (Mult == '*' || Mult == '/')
+                            tbxX.getText().delete(tbxX.getText().length() - 2, tbxX.getText().length());
+                        else
+                            tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
+                    }
+                    if(Mult != '(')
+                    tbxX.getText().append('+');
+                }
                 break;
             case R.id.btDel:
                 if(tbxX.getText().length()>0) {
                     tbxX.getText().delete(tbxX.getText().length() - 1, tbxX.getText().length());
-                    tbxResult.setText(stringFromJNI(tbxX.getText().toString()));
+                    getOperation();
                 }
                 break;
             case R.id.btDelAll:
                 tbxX.getText().clear();
                 tbxResult.getText().clear();
+                tbxResult.setText("0");
                 break;
             case R.id.btIgual:
                 tbxX.getText().clear();
@@ -178,5 +260,40 @@ public class MainActivity extends AppCompatActivity implements android.view.View
      */
     public native String stringFromJNI(String str);
 
+    public boolean IsParentesis()
+    {
+        int cout = NumParentesis();
+        return (cout<=0)?false:true;
+    }
 
+    public int NumParentesis()
+    {
+        String str=tbxX.getText().toString();
+        int cout=0;
+        for(int i =0; i<str.length();i++)
+        {
+            if(str.charAt(i)=='(')
+            {
+                cout++;
+            }
+            else if(str.charAt(i)==')')
+            {
+                cout--;
+            }
+        }
+        return cout;
+    }
+    public void getOperation()
+    {
+        int N = NumParentesis();
+        String str="";
+        for(int i=0;i<N;i++)
+        {
+            str+= ")";
+        }
+        if(tbxX.getText().toString().isEmpty()==false)
+            tbxResult.setText(stringFromJNI(tbxX.getText().toString()+str));
+        else
+            tbxResult.setText("0");
+    }
 }
