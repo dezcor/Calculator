82ed847fd2337f1167568b4c2b0d04a3bb873570
Rivert97 <betogarcia97@live.com.mx>
2018-03-12 00:20:27 -0600

    Otra actividad para prueba con calculadora simple

---
+++ app/src/main/java/com/example/dezcorjm/pruebasnk/SimpleCalc.java
@@ -0,0 +1,13 @@
+package com.example.dezcorjm.pruebasnk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_calc);
+    }
+}
